5a12840220dd8800084fcb9f78d23bf7bd266971
Gözdesu Biçici <gozdesu.bicici@bilgeadamboost.com>
2021-10-31 23:21:20 +0300

    Add project files.

---
+++ BootstrapSiteTasarimi/js/iletisimFormu.js
@@ -0,0 +1,26 @@
+﻿var form = document.querySelector("form");
+var ad = form.elements.txtAdSoyad;
+var mail = form.elements.txtEposta;
+var message = form.elements.txtMesaj;
+
+function validateForm() {
+
+    var hataSayaci = 0;
+    if (ad.value == "") {
+        document.getElementsByName("txtAdSoyad").innerHTML = "Lütfen isim giriniz.";
+        hataSayaci++;
+    }
+    if (mail.value == "") {
+        document.getElementsByName("txtEposta").innerHTML = "Lütfen eposta giriniz.";
+        hataSayaci++;
+    }
+    if (message.value == "") {
+        document.getElementsByName("txtMesaj").innerHTML = "Lütfen mesajınızı giriniz.";
+        hataSayaci++;
+    }   
+    else {
+        return true;
+
+    }
+        
+}
